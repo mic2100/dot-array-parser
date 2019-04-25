60e086351f74e1b637d78e2b28040615286d94ed
Michael Bardsley <mic.bardsley@outlook.com>
2019-04-25 21:01:17 +0100

    Added the docblocks

--- src/Parser.php
+++ src/Parser.php
@@ -2,17 +2,43 @@
 
 namespace Mic2100\DotArrayParser;
 
+use InvalidArgumentException;
+
+/**
+ * Class Parser
+ *
+ * @package Mic2100\DotArrayParser
+ * @author Mike Bardsley
+ * @license MIT
+ */
 class Parser
 {
+    /**
+     * Parse the syntax and return the value from the array
+     *
+     * @param string $syntax
+     * @param array $array
+     * @throws InvalidArgumentException - if the syntax is empty
+     *
+     * @return mixed
+     */
     public function handle(string $syntax, array $array)
     {
         if (empty($syntax)) {
-            throw new \InvalidArgumentException('You need to pass some array syntax');
+            throw new InvalidArgumentException('You need to pass some array syntax');
         }
 
         return $this->getValue($this->getKeys($syntax), $array);
     }
 
+    /**
+     * Get the keys using the dot syntax
+     *
+     * @param string $syntax
+     * @throws InvalidArgumentException - if there are no keys in the syntax
+     *
+     * @return array
+     */
     private function getKeys(string $syntax): array
     {
         $keys = [];
@@ -21,18 +47,27 @@ class Parser
         }
 
         if (empty($keys)) {
-            throw new \InvalidArgumentException('There are no keys passed in the syntax: ' . var_export($syntax, true));
+            throw new InvalidArgumentException('There are no keys passed in the syntax: ' . var_export($syntax, true));
         }
 
         return $keys;
     }
 
+    /**
+     * Get the value from the array using the keys
+     *
+     * @param array $keys
+     * @param array $array
+     * @throws InvalidArgumentException - if the key does not exist
+     *
+     * @return array|mixed
+     */
     private function getValue(array $keys, array $array)
     {
         $value = $array;
         foreach ($keys as $key) {
             if (!isset($value[$key]) || !array_key_exists($key, $value)) {
-                throw new \InvalidArgumentException('The key does not exist: ' . $key);
+                throw new InvalidArgumentException('The key does not exist: ' . $key);
             }
 
             $value = $value[$key];
